cf0319f806253b74ddbb549a64634d22b91869e8
juntingh <junting.huang@gmail.com>
2017-06-12 15:02:04 -0500

    added TPad and TLatex

--- rootalias.py
+++ rootalias.py
@@ -1,4 +1,4 @@
-from ROOT import TH1D, TH2D, TCanvas, gPad, TLegend, gStyle, TGraph, kBlack, kRed, kBlue, kGreen, kOrange, TColor, TPaveStats, TFile, TF1
+from ROOT import TH1D, TH2D, TCanvas, gPad, TLegend, gStyle, TGraph, kBlack, kRed, kBlue, kGreen, kOrange, TColor, TPaveStats, TFile, TF1, TPad, TLatex
 import numpy as np
 
 
